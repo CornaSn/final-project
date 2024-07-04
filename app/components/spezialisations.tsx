ca27b6e0ff70bec2981453f56bf5bfbb9a58bf75
CornaSn <cornelia.schenk@hotmail.com>
2024-07-04 12:00:12 +0200

    Add styling to createExpertProfile

--- app/components/spezialisations.tsx
+++ app/components/spezialisations.tsx
@@ -1,21 +1,14 @@
 import React, { useState } from 'react';
-import type { Selection } from 'react-aria-components';
-import { ListBox, ListBoxItem } from 'react-aria-components';
-
-const selected: string[] = ['item1', 'item2', 'item3'];
 
 export default function Example() {
   const [selectedItems, setSelectedItems] = useState<string[]>([]);
 
-  // Funktion zum Hinzufügen oder Entfernen eines Elements aus der Auswahl
   const toggleSelection = (item: string) => {
     if (selectedItems.includes(item)) {
-      // Element entfernen, wenn es bereits ausgewählt ist
       setSelectedItems(
         selectedItems.filter((selectedItem) => selectedItem !== item),
       );
     } else {
-      // Element hinzufügen, wenn es noch nicht ausgewählt ist
       setSelectedItems([...selectedItems, item]);
     }
   };
@@ -23,14 +16,13 @@ export default function Example() {
   const sandwichContents = ['cheese', 'lettuce', 'tomato', 'bacon'];
 
   return (
-    <div className="container mx-auto mt-10 p-6 bg-white rounded-lg shadow-md">
-      <h1 className="text-2xl font-bold mb-4">Select Sandwich Contents</h1>
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Select expert area:</h1>
 
-      {/* Anzeige der ausgewählten Elemente als Buttons */}
       <div className="flex flex-wrap gap-2 mb-4">
         {selectedItems.map((item) => (
           <button
-            key={item}
+            key={`item-${item}`}
             type="button"
             className="bg-blue-500 text-white px-3 py-1 rounded-full"
             onClick={() => toggleSelection(item)}
@@ -40,15 +32,14 @@ export default function Example() {
         ))}
       </div>
 
-      {/* Anzeige der verfügbaren Elemente zur Auswahl */}
       <div className="flex flex-wrap gap-2 mb-4">
         {sandwichContents.map((item) => (
           <button
-            key={item}
+            key={`item-${item}`}
             type="button"
             className={`bg-gray-300 text-gray-700 px-3 py-1 rounded-full ${selectedItems.includes(item) ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'}`}
             onClick={() => toggleSelection(item)}
-            disabled={selectedItems.includes(item)} // Deaktiviert den Button, wenn das Element bereits ausgewählt ist
+            disabled={selectedItems.includes(item)}
           >
             {item.charAt(0).toUpperCase() + item.slice(1)}
           </button>
--- app/profile/createExpertProfile/CreateExpertProfileForm.tsx
+++ app/profile/createExpertProfile/CreateExpertProfileForm.tsx
@@ -3,138 +3,147 @@
 import { useListBox, useListBoxSection, useOption } from '@react-aria/listbox';
 import { Item, Section } from '@react-stately/collections';
 import { useListState } from '@react-stately/list';
-import React from 'react';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+import Select from 'react-select';
+import { CreateExpertProfileRequestBody } from '../../api/expertProfile/route';
 import Example from '../../components/spezialisations';
+import Experts from '../../experts/page';
 
-// import { useRouter } from 'next/navigation';
-// import React, { useState } from 'react';
-// import Select from 'react-select';
-// import { CreateExpertProfileRequestBody } from '../../api/expertProfile/route';
-// type Props = {
-//   userId: number | undefined;
-// };
-// export default function CreateExpertProfileForm(props: Props) {
-//   const [age, setAge] = useState('');
-//   const [city, setCity] = useState('');
-//   const [bio, setBio] = useState('');
-//   const [pictureUrl, setPictureUrl] = useState('');
-//   const [videoUrl, setVideoUrl] = useState('');
-//   const [travelBlogUrl, setTravelBlogUrl] = useState('');
-//   const maxOptions = 6;
-//   const [selectedSpecializations, setSelectedSpecializations] = useState();
-//   const router = useRouter();
-//   async function handleProfileCreation(
-//     event: React.FormEvent<HTMLFormElement>,
-//   ) {
-//     event.preventDefault();
-//     try {
-//       const response = await fetch('/api/expertProfile', {
-//         method: 'POST',
-//         body: JSON.stringify({
-//           userId: props.userId,
-//           age,
-//           city,
-//           bio,
-//           pictureUrl,
-//           videoUrl,
-//           travelBlogUrl,
-//           // selectedSpecializations: selectedSpecializations.map(
-//           //   (option) => option.value,
-//           // ),
-//         }),
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//       });
-//       const data: CreateExpertProfileRequestBody = await response.json();
-//       console.log('data', data);
-//     } catch (error) {
-//       console.error('An error occurred during profile creation:', error);
-//     }
-//     router.push(`/profile/}`);
-//     router.refresh();
-//   }
-//   return (
-//     <form onSubmit={handleProfileCreation}>
-//       <div className="flex justify-center items-center min-h-screen bg-gray-100">
-//         <div className="w-full max-w-md p-6 bg-white rounded shadow-md space-y-4">
-//           <h1 className="text-2xl font-bold text-center mb-6">
-//             Create Expert Profile
-//           </h1>
-//           <label className="flex flex-col gap-1">
-//             <span className="text-sm font-medium text-gray-700">Age:</span>
-//             <input
-//               className="input input-bordered grow"
-//               placeholder="Age"
-//               onChange={(event) => setAge(event.currentTarget.value)}
-//             />
-//           </label>
-//           <label className="flex flex-col gap-1">
-//             <span className="text-sm font-medium text-gray-700">City: </span>
-//             <input
-//               className="input input-bordered grow"
-//               placeholder="City"
-//               onChange={(event) => setCity(event.currentTarget.value)}
-//             />
-//           </label>
-//           <label className="flex flex-col gap-1">
-//             <span className="text-sm font-medium text-gray-700">
-//               Short Bio:
-//             </span>
-//             <textarea
-//               className="input input-bordered grow h-40 resize-y"
-//               placeholder="Bio"
-//               maxLength={200}
-//               onChange={(event) => setBio(event.currentTarget.value)}
-//             />
-//           </label>
-//           <label className="flex flex-col gap-1">
-//             <span className="text-sm font-medium text-gray-700">
-//               Travel Blog:{' '}
-//             </span>
-//             <input
-//               className="input input-bordered grow"
-//               placeholder="Link to your travel blog"
-//               onChange={(event) => setTravelBlogUrl(event.currentTarget.value)}
-//             />
-//           </label>
-//           <label className="flex flex-col gap-1">
-//             <span className="text-sm font-medium text-gray-700">Picture:</span>
-//             <input
-//               type="file"
-//               className="file-input file-input-bordered w-full max-w-xs"
-//               placeholder="Upload a picture"
-//               onChange={(event) => setPictureUrl(event.currentTarget.value)}
-//             />
-//           </label>
-//           <label className="flex flex-col gap-1">
-//             <span className="text-sm font-medium text-gray-700">Video: </span>
-//             <input
-//               type="file"
-//               className="file-input file-input-bordered w-full max-w-xs"
-//               placeholder="Upload a video"
-//               onChange={(event) => setVideoUrl(event.currentTarget.value)}
-//             />
-//           </label>
-//           <span className="text-sm font-medium text-gray-700">
-//             Favorite Flavor:
-//           </span>
-//           <div className="flex flex-col gap-1">
-//             {' '}
-//             <button className="btn">Upload</button>
-//           </div>
-//         </div>
-//       </div>
-//     </form>
-//   );
-// }
+type Props = {
+  userId: number | undefined;
+};
+export default function CreateExpertProfileForm(props: Props) {
+  const [age, setAge] = useState('');
+  const [city, setCity] = useState('');
+  const [bio, setBio] = useState('');
+  const [pictureUrl, setPictureUrl] = useState('');
+  const [videoUrl, setVideoUrl] = useState('');
+  const [travelBlogUrl, setTravelBlogUrl] = useState('');
+  const maxOptions = 6;
+  const [selectedSpecializations, setSelectedSpecializations] = useState();
+  const router = useRouter();
+  async function handleProfileCreation(
+    event: React.FormEvent<HTMLFormElement>,
+  ) {
+    event.preventDefault();
+    try {
+      const response = await fetch('/api/expertProfile', {
+        method: 'POST',
+        body: JSON.stringify({
+          userId: props.userId,
+          age,
+          city,
+          bio,
+          pictureUrl,
+          videoUrl,
+          travelBlogUrl,
+          // selectedSpecializations: selectedSpecializations.map(
+          //   (option) => option.value,
+          // ),
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const data: CreateExpertProfileRequestBody = await response.json();
+      console.log('data', data);
+    } catch (error) {
+      console.error('An error occurred during profile creation:', error);
+    }
+    router.push(`/profile/}`);
+    router.refresh();
+  }
 
-export default function Test() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Example />
-      </header>
-    </div>
+    <form
+      onSubmit={handleProfileCreation}
+      className="flex justify-center items-center min-h-screen bg-gray-100"
+    >
+      <div className="w-full max-w-4xl p-8 bg-white rounded shadow-md space-y-6">
+        <h1 className="text-3xl font-bold text-center mb-8">
+          Create Expert Profile
+        </h1>
+
+        <div className="grid grid-cols-2 gap-8">
+          <div className="space-y-4">
+            <label className="flex flex-col space-y-2">
+              <span className="text-lg font-medium text-gray-700">Age:</span>
+              <input
+                className="input input-bordered w-full py-3 px-4"
+                placeholder="Age"
+                onChange={(event) => setAge(event.currentTarget.value)}
+              />
+            </label>
+
+            <label className="flex flex-col space-y-2">
+              <span className="text-lg font-medium text-gray-700">City:</span>
+              <input
+                className="input input-bordered w-full py-3 px-4"
+                placeholder="City"
+                onChange={(event) => setCity(event.currentTarget.value)}
+              />
+            </label>
+
+            <label className="flex flex-col space-y-2">
+              <span className="text-lg font-medium text-gray-700">
+                Short Bio:
+              </span>
+              <textarea
+                className="input input-bordered w-full h-48 resize-none py-3 px-4"
+                placeholder="Bio"
+                maxLength={200}
+                onChange={(event) => setBio(event.currentTarget.value)}
+              />
+            </label>
+
+            <label className="flex flex-col space-y-2">
+              <span className="text-lg font-medium text-gray-700">
+                Travel Blog:
+              </span>
+              <input
+                className="input input-bordered w-full py-3 px-4"
+                placeholder="Link to your travel blog"
+                onChange={(event) =>
+                  setTravelBlogUrl(event.currentTarget.value)
+                }
+              />
+            </label>
+          </div>
+
+          <div className="space-y-4">
+            <label className="flex flex-col space-y-2">
+              <span className="text-lg font-medium text-gray-700">
+                Picture:
+              </span>
+              <input
+                type="file"
+                className="file-input file-input-bordered w-full max-w-xs py-3 px-4"
+                placeholder="Upload a picture"
+                onChange={(event) => setPictureUrl(event.currentTarget.value)}
+              />
+            </label>
+
+            <label className="flex flex-col space-y-2">
+              <span className="text-lg font-medium text-gray-700">Video:</span>
+              <input
+                type="file"
+                className="file-input file-input-bordered w-full max-w-xs py-3 px-4"
+                placeholder="Upload a video"
+                onChange={(event) => setVideoUrl(event.currentTarget.value)}
+              />
+            </label>
+
+            <Example />
+          </div>
+        </div>
+
+        <div className="flex justify-center mt-8">
+          <button className="btn w-full md:w-2/3 lg:w-1/2 xl:w-1/3">
+            Upload
+          </button>
+        </div>
+      </div>
+    </form>
   );
 }
