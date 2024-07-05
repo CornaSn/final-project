d619a91f56b2afb3467e5df3385b2c51a17d0b81
CornaSn <cornelia.schenk@hotmail.com>
2024-07-05 10:42:28 +0200

    Modify selection at create expert profile

--- app/components/selectCountries.tsx
+++ app/components/selectCountries.tsx
@@ -7,6 +7,7 @@ type Props = {
 
 export default function SelectCountry(props: Props) {
   const [selectedItems, setSelectedItems] = useState<string[]>([]);
+  const [selectedCountry, setSelectedCountry] = useState('');
 
   const expertCountries = props.expertCountries;
 
@@ -22,12 +23,18 @@ export default function SelectCountry(props: Props) {
     }
   };
 
+  const handleChange = (event) => {
+    const country = event.target.value;
+    setSelectedCountry(country);
+    toggleSelection(country);
+    setSelectedCountry('');
+  };
+
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4">
         Select countries you have visited
       </h1>
-
       <div className="flex flex-wrap gap-2 mb-4">
         {selectedItems.map((item) => (
           <button
@@ -40,20 +47,62 @@ export default function SelectCountry(props: Props) {
           </button>
         ))}
       </div>
-
-      <div className="flex flex-wrap gap-2 mb-4">
-        {countries.map((country) => (
-          <button
-            key={`country-${country}`}
-            type="button"
-            className={`bg-gray-300 text-gray-700 px-3 py-1 rounded-full ${selectedItems.includes(country) ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'}`}
-            onClick={() => toggleSelection(country)}
-            disabled={selectedItems.includes(country)}
-          >
-            {country.charAt(0).toUpperCase() + country.slice(1)}
-          </button>
-        ))}
+      <div className="mb-4">
+        <select
+          value={selectedCountry}
+          onChange={handleChange}
+          className="bg-gray-300 text-gray-700 px-3 py-1 rounded-full"
+        >
+          <option value="" disabled>
+            Choose a country
+          </option>
+          {countries.map((country) => (
+            <option
+              key={`country-${country}`}
+              value={country}
+              disabled={selectedItems.includes(country)}
+            >
+              {country.charAt(0).toUpperCase() + country.slice(1)}
+            </option>
+          ))}
+        </select>
       </div>
     </div>
   );
 }
+
+/*
+//   return (
+//     <div>
+//       <h1 className="text-2xl font-bold mb-4">
+//         Select countries you have visited
+//       </h1>
+//       <div className="flex flex-wrap gap-2 mb-4">
+//         {selectedItems.map((item) => (
+//           <button
+//             key={`item-${item}`}
+//             type="button"
+//             className="bg-blue-500 text-white px-3 py-1 rounded-full"
+//             onClick={() => toggleSelection(item)}
+//           >
+//             {item.charAt(0).toUpperCase() + item.slice(1)}
+//           </button>
+//         ))}
+//       </div>
+
+//       <div className="flex flex-wrap gap-2 mb-4">
+//         {countries.map((country) => (
+//           <button
+//             key={`country-${country}`}
+//             type="button"
+//             className={`bg-gray-300 text-gray-700 px-3 py-1 rounded-full ${selectedItems.includes(country) ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'}`}
+//             onClick={() => toggleSelection(country)}
+//             disabled={selectedItems.includes(country)}
+//           >
+//             {country.charAt(0).toUpperCase() + country.slice(1)}
+//           </button>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// } */
--- app/components/selectExpertise.tsx
+++ app/components/selectExpertise.tsx
@@ -7,10 +7,13 @@ type Props = {
 
 export default function SelectExpertise(props: Props) {
   const [selectedItems, setSelectedItems] = useState<string[]>([]);
+  const [selectedExpertise, setSelectedExpertise] = useState('');
 
-  const expertAreas = props.expertAreas;
+  const expertExpertise = props.expertAreas;
 
-  const expertiseName = expertAreas.map((area) => area.expertiseName);
+  const expertiseName = expertExpertise.map(
+    (expertise) => expertise.expertiseName,
+  );
 
   const toggleSelection = (item: string) => {
     if (selectedItems.includes(item)) {
@@ -22,10 +25,16 @@ export default function SelectExpertise(props: Props) {
     }
   };
 
+  const handleChange = (event) => {
+    const expertise = event.target.value;
+    setSelectedExpertise(expertise);
+    toggleSelection(expertise);
+    setSelectedExpertise('');
+  };
+
   return (
     <div>
-      <h1 className="text-2xl font-bold mb-4">Select expert area:</h1>
-
+      <h1 className="text-2xl font-bold mb-4">Select expert areas:</h1>
       <div className="flex flex-wrap gap-2 mb-4">
         {selectedItems.map((item) => (
           <button
@@ -38,20 +47,60 @@ export default function SelectExpertise(props: Props) {
           </button>
         ))}
       </div>
-
-      <div className="flex flex-wrap gap-2 mb-4">
-        {expertiseName.map((area) => (
-          <button
-            key={`area-${area}`}
-            type="button"
-            className={`bg-gray-300 text-gray-700 px-3 py-1 rounded-full ${selectedItems.includes(area) ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'}`}
-            onClick={() => toggleSelection(area)}
-            disabled={selectedItems.includes(area)}
-          >
-            {area.charAt(0).toUpperCase() + area.slice(1)}
-          </button>
-        ))}
+      <div className="mb-4">
+        <select
+          value={selectedExpertise}
+          onChange={handleChange}
+          className="bg-gray-300 text-gray-700 px-3 py-1 rounded-full"
+        >
+          <option value="" disabled>
+            Choose an expert area
+          </option>
+          {expertiseName.map((expertise) => (
+            <option
+              key={`expertise-${expertise}`}
+              value={expertise}
+              disabled={selectedItems.includes(expertise)}
+            >
+              {expertise.charAt(0).toUpperCase() + expertise.slice(1)}
+            </option>
+          ))}
+        </select>
       </div>
     </div>
   );
 }
+
+//   return (
+//     <div>
+//       <h1 className="text-2xl font-bold mb-4">Select expert area:</h1>
+
+//       <div className="flex flex-wrap gap-2 mb-4">
+//         {selectedItems.map((item) => (
+//           <button
+//             key={`item-${item}`}
+//             type="button"
+//             className="bg-blue-500 text-white px-3 py-1 rounded-full"
+//             onClick={() => toggleSelection(item)}
+//           >
+//             {item.charAt(0).toUpperCase() + item.slice(1)}
+//           </button>
+//         ))}
+//       </div>
+
+//       <div className="flex flex-wrap gap-2 mb-4">
+//         {expertiseName.map((area) => (
+//           <button
+//             key={`area-${area}`}
+//             type="button"
+//             className={`bg-gray-300 text-gray-700 px-3 py-1 rounded-full ${selectedItems.includes(area) ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'}`}
+//             onClick={() => toggleSelection(area)}
+//             disabled={selectedItems.includes(area)}
+//           >
+//             {area.charAt(0).toUpperCase() + area.slice(1)}
+//           </button>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
