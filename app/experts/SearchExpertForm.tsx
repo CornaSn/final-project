d6f4a65878f2f37a27d32e24561e58b3986f7efb
CornaSn <cornelia.schenk@hotmail.com>
2024-07-09 11:59:54 +0200

    Insert Selections to expert search page

--- app/experts/SearchExpertForm.tsx
+++ app/experts/SearchExpertForm.tsx
@@ -2,22 +2,23 @@
 
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { string } from 'zod';
 import { Country } from '../../migrations/00004-createCountriesTable';
 import { Expertise } from '../../migrations/00008-createExpertiseTable';
-import SelectCountry from '../components/selectCountries';
-import SelectExpertise from '../components/selectExpertise';
 
 type Props = {
-  expertAreas: Expertise[];
-  expertCountries: Country[];
+  areasOfInterestName: string[];
+  countryOfInterestName: string[];
 };
 
 export default function ExpertsForm(props: Props) {
-  const [selectedItemsCountries, setSelectedItemsCountries] = useState([]);
-  const [selectedItemsExpertise, setSelectedItemsExpertise] = useState<
-    string[]
-  >([]);
-  const router = useRouter();
+  // console.log('PROOOOOOOOOOOOOOOOOOOOOOOOOPS', props);
+
+  const [selectedCountry, setSelectedCountry] = useState('');
+  const [selectedAreas, setSelectedAreas] = useState(['']);
+
+  // const [areasOfInterest, setareasOfInterest] = useState<string[]>([]);
+  // const router = useRouter();
 
   return (
     <form className="flex justify-center pt-12 min-h-screen bg-gray-100">
@@ -31,27 +32,59 @@ export default function ExpertsForm(props: Props) {
               <h2 className="text-lg font-bold mb-2">
                 Select countries you have visited
               </h2>
-              <SelectCountry
-                expertCountries={props.expertCountries}
-                setSelectedItemsCountries={setSelectedItemsCountries as never}
-                selectedItemsCountries={selectedItemsCountries}
-              />
+              <select
+                value={selectedCountry}
+                // onChange={handleCountryChange}
+                className="bg-gray-300 text-gray-700 px-5 py-2 rounded-full w-full text-center"
+              >
+                <option value="" disabled>
+                  Choose a country
+                </option>
+                {props.countryOfInterestName.map((country) => (
+                  <option key={`country-${country}`} value={country}>
+                    {country}
+                  </option>
+                ))}
+              </select>
             </div>
             <div className="w-1/2">
-              <h2 className="text-lg font-bold mb-2">Select expert areas</h2>
-              <SelectExpertise
-                expertAreas={props.expertAreas}
-                setSelectedItemsExpertise={setSelectedItemsExpertise}
-                selectedItemsExpertise={selectedItemsExpertise}
-              />
+              <h1 className="text-2xl font-bold mb-4">Select expert areas:</h1>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {props.areasOfInterestName.map((area) => (
+                  <button
+                    key={`area-${area}`}
+                    type="button"
+                    className={`bg-blue-500 text-white px-3 py-1 rounded-full ${props.areasOfInterestName.includes(area) ? 'bg-blue-700' : ''}`}
+                    // onClick={() => ()}
+                  >
+                    {area}
+                  </button>
+                ))}
+              </div>
+              <div className="mb-4">
+                <select
+                  value=""
+                  className="bg-gray-300 text-gray-700 px-5 py-2 rounded-full w-full text-center"
+                >
+                  <option value="" disabled>
+                    Choose an expert area
+                  </option>
+                  {props.areasOfInterestName.map((area) => (
+                    <option
+                      key={`area-${area}`}
+                      value={area}
+                      disabled={props.areasOfInterestName.includes(area)}
+                    >
+                      {area}
+                    </option>
+                  ))}
+                </select>
+              </div>
             </div>
           </div>
         </div>
         <div className="flex justify-center mt-4">
-          <button
-            type="submit"
-            className="btn w-full md:w-2/3 lg:w-1/2 xl:w-1/3"
-          >
+          <button className="btn w-full md:w-2/3 lg:w-1/2 xl:w-1/3">
             Search
           </button>
         </div>
--- app/experts/page.tsx
+++ app/experts/page.tsx
@@ -12,13 +12,19 @@ export default async function SearchExperts() {
   const userId = await userWithValidSession();
 
   // Fetch select fields for user profile
-  const expertAreas = await getExpertiseListInsecure();
-  const expertCountries = await getCountriesListInsecure();
+  const areasOfInterest = await getExpertiseListInsecure();
+  const areasOfInterestName = areasOfInterest.map((area) => area.expertiseName);
+
+  const countryOfInterest = await getCountriesListInsecure();
+  const countryOfInterestName = countryOfInterest.map(
+    (country) => country.countryName,
+  );
+  console.log('countryOfInterestName', countryOfInterestName);
 
   return (
     <SearchExpertsForm
-      expertAreas={expertAreas}
-      expertCountries={expertCountries}
+      areasOfInterestName={areasOfInterestName}
+      countryOfInterestName={countryOfInterestName}
     />
   );
 }
