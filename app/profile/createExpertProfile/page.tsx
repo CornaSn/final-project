40e50d0a9c94a437c64c596922d7ce6d06017244
CornaSn <cornelia.schenk@hotmail.com>
2024-07-11 13:37:57 +0200

    Insert picture upload to createProfileForm

--- app/profile/createExpertProfile/page.tsx
+++ app/profile/createExpertProfile/page.tsx
@@ -1,6 +1,5 @@
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
-import cloudinary from '../../../cloudinary.config';
 import { getCountriesListInsecure } from '../../../database/countriesList';
 import { getExpertiseListInsecure } from '../../../database/expertiseList';
 import { getExpertWithUserById } from '../../../database/experts';
